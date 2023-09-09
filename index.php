cf2c46e95d775a6232a1b8aa0a38c7bfd59ed060
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-09-09 20:35:42 -0300

    Arrumei alguns códigos no PHP

--- index.php
+++ index.php
@@ -1,33 +1,29 @@
 <?php
 
-// Passando os dados obtidos pelo formulário para as variáveis abaixo
 $nomeremetente     = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
 $emailremetente    = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL, FILTER_SANITIZE_SPECIAL_CHARS));
-$emaildestinatario = 'profissionalmurillogomes@gmail.com'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
-$mensagem             = filter_input(INPUT_POST, "MENSAGEM", FILTER_SANITIZE_SPECIAL_CHARS);
+$emaildestinatario = 'profissionalmurillogomes@gmail.com'; // Seu e-mail de destino
+$mensagem          = filter_input(INPUT_POST, "MENSAGEM", FILTER_SANITIZE_SPECIAL_CHARS);
+$assunto           = 'Assunto do E-mail'; // Defina o assunto aqui
 
-
-/* Mensagem a ser enviada no corpo do e-mail. */
 $mensagemHTML = '<strong>Formulário de Contato</strong>
-<p><b>Nome:</b> ' . $nomeremetente . '
-<p><b>E-Mail:</b> ' . $emailremetente . '
-<p><b>Assunto:</b> ' . $assunto . '
+<p><b>Nome:</b> ' . $nomeremetente . '</p>
+<p><b>E-Mail:</b> ' . $emailremetente . '</p>
+<p><b>Assunto:</b> ' . $assunto . '</p>
 <p><b>Mensagem:</b> ' . $mensagem . '</p>
 <hr>';
 
-
-// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
-// O return-path deve ser ser o mesmo e-mail do remetente.
-$headers = "MIME-Version: 1.1\r\n";
+$headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
-$headers .= "From: $emailremetente\r\n"; // remetente
-$headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
+$headers .= "From: $emailremetente\r\n";
+$headers .= "Return-Path: $emailremetente\r\n";
+
 $envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers);
 
-if ($enviaremail) {
-    $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-    echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+if ($envio) {
+    $mgm = "E-MAIL ENVIADO COM SUCESSO! O link será enviado para o e-mail fornecido no formulário.";
+    echo "<meta http-equiv='refresh' content='10;URL=contato.php'>";
 } else {
     $mgm = "ERRO AO ENVIAR E-MAIL!";
-    echo "";
+    echo $mgm;
 }
